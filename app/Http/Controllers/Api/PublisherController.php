9198bbe66d67eaae7737c8cc44aedba47898eeeb
theonlyshannon <sofwannuhaalfaruq@gmail.com>
2025-02-10 14:30:43 +0700

    feat: publisher

--- app/Http/Controllers/Api/PublisherController.php
+++ app/Http/Controllers/Api/PublisherController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PublisherResource;
+use App\Http\Requests\PublisherStoreRequest;
+use App\Http\Requests\PublisherUpdateRequest;
 
 class PublisherController extends Controller
 {
@@ -17,14 +19,22 @@ class PublisherController extends Controller
     {
         $publisher = Publisher::all();
 
-        return ResponseHelper::jsonResponse(true, 'Categories fetched successfully', PublisherResource::collection($publisher), 200);
+        return ResponseHelper::jsonResponse(true, 'Publisher fetched successfully', PublisherResource::collection($publisher), 200);
     }
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PublisherStoreRequest $request)
     {
-        //
+        $request = $request->validated();
+
+        $publisher = new Publisher();
+        $publisher->name = $request['name'];
+        $publisher->slug = $request['address'];
+        $publisher->save();
+
+        return ResponseHelper::jsonResponse(true, 'Publisher create successfully', PublisherResource::collection($publisher), 201);
+
     }
 
     /**
@@ -32,23 +42,31 @@ class PublisherController extends Controller
      */
     public function show(string $id)
     {
-        //
-    }
+        $publisher = Publisher::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        if(!$publisher) {
+            return ResponseHelper::jsonResponse(false, 'Publisher not found', null, 404);
+        }
+
+        return ResponseHelper::jsonResponse(true, 'Publisher fetched successfully', PublisherResource::collection($publisher), 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(PublisherUpdateRequest $request, string $id)
     {
-        //
+        $publisher = Publisher::find($id);
+
+        if(!$publisher) {
+            return ResponseHelper::jsonResponse(false, 'Publisher not found', null, 404);
+        }
+
+        $publisher->name = $request['name'];
+        $publisher->slug = $request['address'];
+        $publisher->save();
+
+        return ResponseHelper::jsonResponse(true, 'Publisher create successfully', PublisherResource::collection($publisher), 200);
     }
 
     /**
@@ -56,6 +74,14 @@ class PublisherController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $publisher = Publisher::find($id);
+
+        if(!$publisher) {
+            return ResponseHelper::jsonResponse(false, 'Publisher not found', null, 404);
+        }
+
+        $publisher->delete();
+
+        return ResponseHelper::jsonResponse(true, 'Publisher successfully', null, 200);
     }
 }
